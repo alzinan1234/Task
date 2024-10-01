a51b4a030e8c0dc637990056cda245f06048a87d
alzinan1234 <papurohan1100@gmail.com>
2024-10-02 04:02:59 +0600

    add

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import footer from "../assets/Group 3344.png";
 const Footer = () => {
   return (
     <div className="maxWidth mx-auto">
-      <div className="flex justify-between items-center mt-10 mb-10">
+      <div className="flex justify-between items-center mt-5 mb-10">
         <div>
           <p>合計 0 1 件中 0 1 - 10 件を表示</p>
         </div>
